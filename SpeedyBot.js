9bf597ed95664bb5c6fec3a537234392b70f7da1
Zakhersas <11006634+Zakhersas@users.noreply.github.com>
2018-12-02 15:50:27 -0700

    updoots

--- SpeedyBot.js
+++ SpeedyBot.js
@@ -72,8 +72,8 @@ client.on('message', msg => {
 					return msg.reply(`Only server owner can use this command.`);
 				}
 			} else {
-				const role = await msg.guild.roles.find('name', cmd.help.role);
-				const member = await msg.member;
+				const role = msg.guild.roles.find('name', cmd.help.role);
+				const member = msg.member;
 				if (role) {
 					if (role.position > member.highestRole.position) return msg.reply(`You cannot use this command as you do not have the ${cmd.help.role} role`);
 				}
