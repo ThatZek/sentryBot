07f9447ba58a85f8cb1cff26a84e52d431caf110
Zakhersas <11006634+Zakhersas@users.noreply.github.com>
2018-12-02 16:34:00 -0700

    updoots

--- cmds/Admin/promote.js
+++ cmds/Admin/promote.js
@@ -5,15 +5,18 @@ const scoutrole = config.scoutrole;
 
 module.exports.run = async (client, msg, args) => {
     const member = msg.mentions.members.first();
+    const tscout = msg.guild.roles.get(scoutrole[0])
     const scout = msg.guild.roles.get(scoutrole[1]);
     const hscout = msg.guild.roles.get(scoutrole[2]);
         if (member) {
-            if (!member.roles.has(scoutrole[0])) return msg.reply('That person isn\t a scout!')
+            if (!member.roles.has(tscout)) return msg.reply('That person isn\t a scout!')
             if (!member.roles.has(scout)) {
+                member.removeRole(tscout)
                 member.addRole(scout)
                 l.log('promotion', 'Promoted <@' + msg.mentions.users.first().id + '> to scout', client)
                 return msg.react('✅');
             } else if (!member.roles.has(hscout)){
+                member.removeRole(scout)
                 member.addRole(hscout)
                 l.log('promotion', 'Promoted <@' + msg.mentions.users.first().id + '> to head scout', client)
                 return msg.react('✅');
