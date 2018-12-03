57bdd5b74c17efcd8ed40e27ffe521d4b38d30ff
Zakhersas <11006634+Zakhersas@users.noreply.github.com>
2018-12-02 17:44:37 -0700

    upDOOOT

--- cmds/Admin/trial.js
+++ cmds/Admin/trial.js
@@ -1,13 +1,10 @@
-const Discord = require("discord.js");
 const config = require("../../config.json");
-const l = require('../../log.js')
-const scoutrole = config.scoutrole;
 
 module.exports.run = async (client, msg, args) => {
 	const scouter = msg.mentions.members.first();
 	if (!scouter) return msg.reply(`You are incorrectly using the !trialscout command. Please follow up the command with a @UserToPromote`);
 	const trialRole = msg.guild.roles.find('name', 'Trial Scout');
-	if (scouter.roles.exists('name', trialRole.name)) return msg.reply(`${scouter} already has the Trial Scout role`);
+	if (scouter.roles.exists('name', 'Trial Scout')) return msg.reply(`${scouter} already has the Trial Scout role`);
 	scouter.addRole(trialRole);
 	msg.react(`✅`);
 	msg.guild.channels.get('485866359289413632').send(`${scouter} has been promoted to Trial Scout`);
