ee5c25634898b7b93cc9daca11063c53b3d00801
Zakhersas <11006634+Zakhersas@users.noreply.github.com>
2018-12-02 17:22:46 -0700

    updoot

--- cmds/Admin/trial.js
+++ cmds/Admin/trial.js
@@ -4,19 +4,13 @@ const l = require('../../log.js')
 const scoutrole = config.scoutrole;
 
 module.exports.run = async (client, msg, args) => {
-    const member = msg.mentions.members.first();
-    const role = msg.guild.roles.find('name', 'Trial Scout');
-        if (member) {
-            if (!member.roles.has(role.id)) {
-                member.addRole(role.id)
-                l.log('promotion', 'Promoted <@' + msg.mentions.users.first().id + '> to trial scout', client)
-                return msg.react('✅');
-            } else {
-                return msg.reply(' That person already is a scout!');
-            }
-        } else {
-                return msg.reply(' You need to mention someone!');
-        }
+	const scouter = msg.mentions.members.first();
+	if (!scouter) return msg.reply(`You are incorrectly using the !trialscout command. Please follow up the command with a @UserToPromote`);
+	const trialRole = msg.guild.roles.find('name', 'Trial Scout');
+	if (scouter.roles.exists('name', trialRole.name)) return msg.reply(`${scouter} already has the Trial Scout role`);
+	scouter.addRole(trialRole);
+	msg.react(`👍`);
+	msg.guild.channels.get('485866359289413632').send(`${scouter} has been promoted to Trial Scout`);
 }
 
 module.exports.help = {
