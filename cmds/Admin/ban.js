781961d66776650c600aadf9934e3d5a8f4b0b0d
Zakhersas <11006634+Zakhersas@users.noreply.github.com>
2018-12-04 17:23:59 -0700

    yeet

--- cmds/Admin/ban.js
+++ cmds/Admin/ban.js
@@ -4,41 +4,41 @@ const l = require('../../log.js')
 
 module.exports.run = async (client, msg, args) => {
     const user = msg.mentions.users.first();
-args.shift()
-const reason = args.toString(' ')
-if (user) {
-    const member = msg.guild.member(user);
-    if (member) {
-        member.kick(reason).then(() => {
-            msg.reply(`Successfully kicked ${user.tag}`);
-            user.send(`You have been kicked for: ${reason}`)
-
-            l.log('ban', {
-                embed: {
-                    color: 16312092,
-                    title: '**Action:** Kick',
-                    description: 'Reason: ' + reason + '\n User: ' + user.tag,
-                    author: {
-                        name: msg.author.tag,
-                        icon_url: msg.author.icon_url
-                    }
-                }}, client)
-        }).catch(err => {
-            msg.reply('I was unable to kick the member');
-            console.error(err);
-        });
-    } else {
-        msg.reply('That user isn\'t in this server!');
-    }
-} else {
-    msg.reply('You didn\'t mention the user to kick!');
-}
+        args.shift()
+        const reason = args.toString(' ')
+        if (user) {
+            const member = msg.guild.member(user);
+            if (member) {
+                member.ban(reason).then(() => {
+                    msg.reply(`Successfully banned ${user.tag}`);
+                    user.send(`You have been banned for: ${reason}`)
+                    l.log('ban', {
+                        embed: {
+                            color: 16312092,
+                            title: '**Action:** Ban',
+                            description: 'Reason: ' + reason + '\n User: ' + user.tag,
+                            author: {
+                                name: msg.author.tag,
+                                icon_url: msg.author.icon_url
+                            }
+                        }
+                    }, client)
+                }).catch(err => {
+                    msg.reply('I was unable to ban the member');
+                    console.error(err);
+                });
+            } else {
+                msg.reply('That user isn\'t in this server!');
+            }
+        } else {
+            msg.reply('You didn\'t mention the user to ban!');
+        }
 }
 
 module.exports.help = {
-    name: 'ban',
+    name: 'kick',
     role: config.modrole,
     usage: '`User`',
-    desc: `bans mentioned user`,
+    desc: `kicks mentioned user`,
     example: '@That "One" Turtle#1123'
 }
---
+++ cmds/Admin/poll.js
@@ -0,0 +1,6 @@
+const config = require("../../config.json");
+const discord = require("discord.js")
+
+module.exports.run = async (client, msg, args) => {
+
+}
