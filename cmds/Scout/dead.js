02bb56c0aed0b276169d64de185e7261242b823e
Zakhersas <11006634+Zakhersas@users.noreply.github.com>
2018-12-04 19:01:30 -0700

    added counter

--- cmds/Scout/dead.js
+++ cmds/Scout/dead.js
@@ -9,12 +9,30 @@ const output = config.output
 module.exports.run = async (client, msg, args) => {
     if (pubVar.currentSentry === null) return;
     client.channels.get(output).fetchMessage(pubVar.currentSentry).then(msg => {
-        msg.edit({
-            embed: {
-                color: 3447003,
-                title: 'This sentry has been slain!'
-            }
+        msg.delete()
         })
+    killcount = pubVar.killcount.parseInt();
+    pubVar.killcount = killcount + 1;
+    savepubVar();
+    const cloak = bot.emojis.get('431954264785682443');
+    const sentry = bot.emojis.get('431954057691791372');
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+
+    const countMsg = new Discord.RichEmbed()
+        .setAuthor(`Sentry Clean-Up Crew`, bot.user.avatarURL)
+        .setFooter(`Created by That "One" Turtle#1123`)
+        .setThumbnail(msg.guild.iconURL)
+        .setTimestamp(new Date(Date.now()))
+        .setURL('https://discord.gg/x82h6wr')
+        .addField(`**Sentries Killed:** ${sentry}:`, `${config.killcount}`)
+        .addField(`**Cloaks Dropped:** ${cloak}:`, `${config.cloaks}`)
+        .addField(`**Most Calls:**`, `${sentry} ${mostCallerMember} ${sentry}: **${mostCalls}**`)
+        .setColor([red, green, blue]);
+    client.channels.get(output).fetchMessage(config.statsmsg).then(msg => {
+        msg.edit(countMsg);
     })
     pubVar.currentSentry = null;
 }
