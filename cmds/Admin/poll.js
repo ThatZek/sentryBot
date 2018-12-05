9a84307ff3ff261aba3b7eaf7a3f85c9eeffb25d
Zakhersas <11006634+Zakhersas@users.noreply.github.com>
2018-12-04 17:38:16 -0700

    fixed load error with poll.js

--- cmds/Admin/poll.js
+++ cmds/Admin/poll.js
@@ -2,5 +2,13 @@ const config = require("../../config.json");
 const discord = require("discord.js")
 
 module.exports.run = async (client, msg, args) => {
+    msg.reply('**IN DEVELOPMENT!**')
+}
 
+module.exports.help = {
+    name: 'poll',
+    role: config.modrole,
+    usage: '`Question`',
+    desc: `Creates a poll with the question`,
+    example: 'Is this a cool poll?'
 }
